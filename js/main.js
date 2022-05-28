4c9e06878612c64cf04a8ba39fc69884b477f548
Leutbing <spdhsrnvl123@naver.com>
2022-05-28 23:16:34 +0900

    second

--- js/main.js
+++ js/main.js
@@ -19,13 +19,12 @@ const update = () => {
         represent()
     }
 
-
     const represent = () => {
-        // const section_0 = document.querySelector(".section-0");
+        const showing = document.querySelector(".section-0.showing");
         const main_title = document.querySelector(".main-title");
         const first_text = document.querySelector(".first-text");
         const second_text = document.querySelector(".second-text");
-        const showing = document.querySelector(".section-0.showing");
+
 
         if (showing) {
             main_title.classList.add("active");
@@ -36,11 +35,35 @@ const update = () => {
             first_text.classList.remove("active");
             second_text.classList.remove("active");
         }
+        
+        const showing_2 = document.querySelector(".section-1.showing");
+        const section_1_text = document.querySelector(".section-1-text");
+        const section_1_text2 = document.querySelector(".section-1-text2");
+
+        if (showing_2) {
+            section_1_text.classList.add("active");
+            section_1_text2.classList.add("active");
+        } else {
+            section_1_text.classList.remove("active");
+            section_1_text2.classList.remove("active");    
+        }
+        
+        
+        
+        const showing_3 = document.querySelector(".section-2.showing");
+        const section_2_text = document.querySelector(".section-2-text");
+        const section_2_text2 = document.querySelector(".section-2-text2");
+        
+        if (showing_3) {
+            section_2_text.classList.add("active");
+            section_2_text2.classList.add("active");
+        } else {
+            section_2_text.classList.remove("active");
+            section_2_text2.classList.remove("active");
+        }
     }
-
-    carousel()
-    setInterval(carousel, 8000);
-
+        carousel()
+        setInterval(carousel, 8000);
 }
 
 window.addEventListener("load", update);
