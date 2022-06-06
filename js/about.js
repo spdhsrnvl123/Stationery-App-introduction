6083566178a767ef5e479196942d1b358b77c300
Leutbing <spdhsrnvl123@naver.com>
2022-06-06 20:43:40 +0900

    about page finish

--- js/about.js
+++ js/about.js
@@ -2,27 +2,66 @@ const update = () => {
     const section = document.getElementsByTagName("section");
     const text_box = document.querySelectorAll(".text-box");
     const img = document.querySelectorAll(".img-box");
-    const totalNum = text_box.length;
+    const content_1 = document.querySelector(".content-1");
+    const content_2 = document.querySelector(".content-2");
+    const color_container = document.querySelector(".color-container");
     let pageNum;
+
     //전역에 let pageNum = 0
 
     window.addEventListener("scroll", () => {
         let yOffset = window.pageYOffset;
 
-        for (let i = 0; i < totalNum; i++){
-            if (yOffset > section[i+3].offsetTop - window.innerHeight/3 && yOffset < section[i+3].offsetTop  -window.innerHeight/3 + section[i+2].offsetHeight) {
+        for (let i = 0; i < section.length; i++){
+            if (yOffset > section[i].offsetTop - window.innerHeight/3 && yOffset < section[i].offsetTop - window.innerHeight/3 + section[i].offsetHeight) {
                 pageNum = i;
                 break;
             }
         }
-
         pageEffect()
     })
 
+    //섹션별 이벤트 적용
     const pageEffect = () => {
-        text_box[pageNum].style.opacity = 1;
-        text_box[pageNum].style.transform = `none`;
-        img[pageNum].style.opacity = 1;
+        switch (pageNum) {
+            case 0:
+                // console.log(pageNum);
+                break;
+            case 1:
+                // console.log(pageNum);
+                break;
+            case 2:
+                // console.log(pageNum);
+                break;
+            case 3:
+                text_box[0].style.opacity = 1;
+                text_box[0].style.transform = `none`;
+                img[0].style.opacity = 1;
+                break;
+            case 4:
+                text_box[1].style.opacity = 1;
+                text_box[1].style.transform = `none`;
+                img[1].style.opacity = 1;
+                break;
+            case 5:
+                text_box[2].style.opacity = 1;
+                text_box[2].style.transform = `none`;
+                img[2].style.opacity = 1;
+                break;
+            case 6:
+                // console.log(pageNum);
+                break;
+            case 7:
+                content_1.style.opacity = 1;
+                content_1.style.transform = `none`;
+                content_2.style.opacity = 1;
+                content_2.style.transform = `none`;
+                break;
+            case 8:
+                color_container.style.opacity = 1;
+                color_container.style.transform = `none`;
+                break;
+        }
     }
 
 }
